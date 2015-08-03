7df7d98f6b09725da59f2902bffca8f800567e77
Jonathan Neal <jonathantneal@hotmail.com>
2015-08-03 00:02:56 -0400

    Update plugin
    Support font declaration
    Do not alter normal or bold

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 var postcss = require('postcss');
 var fontweights = require('css-font-weight-names');
 
+delete fontweights.normal;
+delete fontweights.bold;
+
+var match = new RegExp('(^|\\s)(' + Object.keys(fontweights).join('|') + ')(\\s|$)');
+
 module.exports = postcss.plugin('postcss-font-weights', function (opts) {
 	opts = opts || {};
 
 	return function (css) {
-		css.eachDecl('font-weight', function (decl) {
-			if (fontweights[decl.value]) {
-				decl.value = fontweights[decl.value];
+		css.eachDecl(/^font(-weight)?$/, function (decl) {
+			if (match.test(decl.value)) {
+				decl.value = decl.value.replace(match, function (all, before, value, after) {
+					return before + fontweights[value] + after;
+				});
 			}
 		});
 	};
--- test/test.js
+++ test/test.js
@@ -14,7 +14,63 @@ var test = function (input, output, opts, done) {
 };
 
 describe('postcss-font-weights', function () {
-	it('transforms font-weight thin', function (done) {
+	it('transforms font: thin 1em', function (done) {
+		test('a{font: thin 1em}', 'a{font: 100 1em}', {}, done);
+	});
+
+	it('transforms font: extralight 1em', function (done) {
+		test('a{font: extralight 1em}', 'a{font: 200 1em}', {}, done);
+	});
+
+	it('transforms font: ultralight 1em', function (done) {
+		test('a{font: ultralight 1em}', 'a{font: 200 1em}', {}, done);
+	});
+
+	it('transforms font: light 1em', function (done) {
+		test('a{font: light 1em}', 'a{font: 300 1em}', {}, done);
+	});
+
+	it('transforms font: book 1em', function (done) {
+		test('a{font: book 1em}', 'a{font: 400 1em}', {}, done);
+	});
+
+	it('transforms font: regular 1em', function (done) {
+		test('a{font: regular 1em}', 'a{font: 400 1em}', {}, done);
+	});
+
+	it('transforms font: roman 1em', function (done) {
+		test('a{font: roman 1em}', 'a{font: 400 1em}', {}, done);
+	});
+
+	it('transforms font: medium 1em', function (done) {
+		test('a{font: medium 1em}', 'a{font: 500 1em}', {}, done);
+	});
+
+	it('transforms font: demibold 1em', function (done) {
+		test('a{font: demibold 1em}', 'a{font: 600 1em}', {}, done);
+	});
+
+	it('transforms font: semibold 1em', function (done) {
+		test('a{font: semibold 1em}', 'a{font: 600 1em}', {}, done);
+	});
+
+	it('transforms font: extrabold 1em', function (done) {
+		test('a{font: extrabold 1em}', 'a{font: 800 1em}', {}, done);
+	});
+
+	it('transforms font: ultrabold 1em', function (done) {
+		test('a{font: ultrabold 1em}', 'a{font: 800 1em}', {}, done);
+	});
+
+	it('transforms font: black 1em', function (done) {
+		test('a{font: black 1em}', 'a{font: 900 1em}', {}, done);
+	});
+
+	it('transforms font: heavy 1em', function (done) {
+		test('a{font: heavy 1em}', 'a{font: 900 1em}', {}, done);
+	});
+
+	it('transforms font-weight: thin', function (done) {
 		test('a{font-weight: thin}', 'a{font-weight: 100}', {}, done);
 	});
 
@@ -32,10 +88,6 @@ describe('postcss-font-weights', function () {
 
 	it('transforms font-weight: book', function (done) {
 		test('a{font-weight: book}', 'a{font-weight: 400}', {}, done);
-	});
-
-	it('transforms font-weight: normal', function (done) {
-		test('a{font-weight: normal}', 'a{font-weight: 400}', {}, done);
 	});
 
 	it('transforms font-weight: regular', function (done) {
@@ -58,10 +110,6 @@ describe('postcss-font-weights', function () {
 		test('a{font-weight: semibold}', 'a{font-weight: 600}', {}, done);
 	});
 
-	it('transforms font-weight: bold', function (done) {
-		test('a{font-weight: bold}', 'a{font-weight: 700}', {}, done);
-	});
-
 	it('transforms font-weight: extrabold', function (done) {
 		test('a{font-weight: extrabold}', 'a{font-weight: 800}', {}, done);
 	});
@@ -76,6 +124,58 @@ describe('postcss-font-weights', function () {
 
 	it('transforms font-weight: heavy', function (done) {
 		test('a{font-weight: heavy}', 'a{font-weight: 900}', {}, done);
+	});
+
+	it('preserves font: normal 1em', function (done) {
+		test('a{font: normal 1em}', 'a{font: normal 1em}', {}, done);
+	});
+
+	it('preserves font: bold 1em', function (done) {
+		test('a{font: bold 1em}', 'a{font: bold 1em}', {}, done);
+	});
+
+	it('preserves font: 100 1em', function (done) {
+		test('a{font: 100 1em}', 'a{font: 100 1em}', {}, done);
+	});
+
+	it('preserves font: 200 1em', function (done) {
+		test('a{font: 200 1em}', 'a{font: 200 1em}', {}, done);
+	});
+
+	it('preserves font: 300 1em', function (done) {
+		test('a{font: 300 1em}', 'a{font: 300 1em}', {}, done);
+	});
+
+	it('preserves font: 400 1em', function (done) {
+		test('a{font: 400 1em}', 'a{font: 400 1em}', {}, done);
+	});
+
+	it('preserves font: 500 1em', function (done) {
+		test('a{font: 500 1em}', 'a{font: 500 1em}', {}, done);
+	});
+
+	it('preserves font: 600 1em', function (done) {
+		test('a{font: 600 1em}', 'a{font: 600 1em}', {}, done);
+	});
+
+	it('preserves font: 700 1em', function (done) {
+		test('a{font: 700 1em}', 'a{font: 700 1em}', {}, done);
+	});
+
+	it('preserves font: 800 1em', function (done) {
+		test('a{font: 800 1em}', 'a{font: 800 1em}', {}, done);
+	});
+
+	it('preserves font: 900 1em', function (done) {
+		test('a{font: 900 1em}', 'a{font: 900 1em}', {}, done);
+	});
+
+	it('preserves font-weight: normal', function (done) {
+		test('a{font-weight: normal}', 'a{font-weight: normal}', {}, done);
+	});
+
+	it('preserves font-weight: bold', function (done) {
+		test('a{font-weight: bold}', 'a{font-weight: bold}', {}, done);
 	});
 
 	it('preserves font-weight: 100', function (done) {
